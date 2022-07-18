2f32b2d25c3eab22eb3b3a5d7c362bb4d8eb5367
@T322309 <aline.granja@copel.com>
2022-07-18 19:56:05 -0300

    Scripts da base de dados.

---
+++ docs/script_database.sql
@@ -0,0 +1,95 @@
+CREATE DATABASE novosigeve
+
+CREATE SCHEMA sigeve
+
+CREATE TABLE sigeve.usuario
+(
+    id bigserial NOT NULL PRIMARY KEY,
+    nome character varying(150),
+    email character varying(100),
+    senha character varying(20),
+	id_ref bigint,
+    data_cadastro date,
+    data_atualizacao date
+)
+
+CREATE TABLE sigeve.unidade
+(
+    id bigserial NOT NULL PRIMARY KEY,
+    descricao character varying(30),
+	codigo_ref character varying(2),
+    data_cadastro date,
+    id_usuario bigint REFERENCES sigeve.usuario (id)
+)
+
+CREATE TABLE sigeve.produto
+(
+    id bigserial NOT NULL PRIMARY KEY,
+    descricao character varying(100),
+    id_unidade bigint REFERENCES sigeve.unidade (id),
+	codigo_ean character varying(13),
+	codigo_ref character varying(6),
+    data_cadastro date,
+    id_usuario bigint REFERENCES sigeve.usuario (id)
+)
+
+CREATE TABLE sigeve.fornecedor
+(
+    id bigserial NOT NULL PRIMARY KEY,
+    nome character varying(100),
+    cpf_cnpj character varying(30),
+    inscricao character varying(20),
+    endereco character varying(60),
+	bairro character varying(40),
+	cep character varying(8),
+	cidade character varying(40),
+	uf character varying(2),
+	telefone character varying(20),
+    email  character varying(100),
+	observacao character varying(100),
+	codigo_ref character varying(6),
+    data_cadastro date,
+    id_usuario bigint REFERENCES sigeve.usuario (id)
+)
+
+CREATE TABLE sigeve.empresa
+(
+    id bigserial NOT NULL PRIMARY KEY,
+    nome character varying(100),
+    cpf_cnpj character varying(30),
+    inscricao character varying(20),
+    endereco character varying(60),
+	bairro character varying(40),
+	cep character varying(8),
+	cidade character varying(40),
+	uf character varying(2),
+	telefone character varying(20),
+    email  character varying(100),
+	observacao character varying(100),
+	codigo_ref character varying(2),
+    data_cadastro date,
+    id_usuario bigint REFERENCES sigeve.usuario (id)
+)
+
+CREATE TABLE sigeve.nota_entrada
+(
+    id bigserial NOT NULL PRIMARY KEY,
+    data_nota date,
+    id_empresa bigint REFERENCES sigeve.empresa (id),
+    id_fornecedor bigint REFERENCES sigeve.fornecedor (id),
+    observacao character varying(100),
+    valor_total numeric(14,2),
+    data_cadastro date,
+	data_atualizacao date,
+    id_usuario bigint REFERENCES sigeve.usuario (id)
+)
+
+CREATE TABLE sigeve.item_nota_entrada
+(
+    id bigserial NOT NULL PRIMARY KEY,
+    id_nota_entrada bigint REFERENCES sigeve.nota_entrada (id),
+    id_produto bigint REFERENCES sigeve.produto (id),
+    qtd numeric(9),
+    preco numeric(10,2),
+    total numeric(10,2)
+)
