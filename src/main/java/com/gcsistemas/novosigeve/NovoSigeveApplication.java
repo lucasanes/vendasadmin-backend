f2d5233736b48beee674849bd14a58600caac24f
alinegiroto <aline.granja@gmail.com>
2022-10-19 16:48:15 -0300

    addCorsMappings

--- src/main/java/com/gcsistemas/novosigeve/NovoSigeveApplication.java
+++ src/main/java/com/gcsistemas/novosigeve/NovoSigeveApplication.java
@@ -4,13 +4,22 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.gcsistemas.novosigeve.util.Ambiente;
 import com.gcsistemas.novosigeve.util.PropriedadesAplicacao;
 
 @SpringBootApplication
-public class NovoSigeveApplication {
+@EnableWebMvc
+public class NovoSigeveApplication implements WebMvcConfigurer {
 
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+	}
+	
 	private static final Logger logger = LoggerFactory.getLogger(NovoSigeveApplication.class);
 	
 	public static void main(String[] args) {
